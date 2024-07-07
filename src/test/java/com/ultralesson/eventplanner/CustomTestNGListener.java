ccfd42dc5252e398eda78f2df6e1294366d53ca8
Sandeep-2 <sandeepsurapalli25@gmail.com>
2024-07-07 13:31:21 +0530

    1.  Enhancing Testing with TestNG Listeners

---
+++ src/test/java/com/ultralesson/eventplanner/CustomTestNGListener.java
@@ -0,0 +1,25 @@
+package com.ultralesson.eventplanner;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class CustomTestNGListener implements ITestListener {
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("Starting test: " + result.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("Test passed: " + result.getName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("Test failed: " + result.getName() + ", Exception: " + result.getThrowable());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("Test skipped: " + result.getName());
+    }
+}
